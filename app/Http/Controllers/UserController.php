2fd2ea28b39d81e49c5c5d84a3bbdd25012365ca
Gyane-git <alpen850@gmail.com>
2025-07-27 17:52:48 +0545

    making api

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function all()
+    {
+        return User::all();
+    }
+
+    // public function adduser(Request $request)
+    // {
+    //     return "User added successfully: " ;
+    // }
+
+    public function adduser(Request $request)
+    {
+        $user = new User();
+        $user->name = $request ->name;
+        $user->email = $request ->email;
+        $user->phone = $request ->phone;
+        $user->save();
+        return response()->json(['message' => 'User added successfully', 'user' => $user], 201);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('test', [TestController::class, 'list']);
+
+
+Route::get('users', [UserController::class, 'all']);
+Route::post('adduser', [UserController::class, 'adduser']);
